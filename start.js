1422f2523ee7064108aa312e2ee0edfd15a35065
HibbaHindawi <hibba04@hotmail.se>
2024-04-17 19:55:30 +0200

    Började med HTML, CSS och Javascript för första sidan

---
+++ start.js
@@ -0,0 +1,24 @@
+let navActive = false; //variabel för att kolla om dropdownmenyn är aktiv eller inte från början, icke-aktiv från början
+let navMenu;
+
+function init() {
+    dropdownBtn = document.querySelector("#dropbtn");
+    dropdownBtn.addEventListener("click", OpenAndCloseNavBar);
+    navMenu = document.querySelector("#dropdown");
+}
+window.addEventListener("load", init);
+
+function OpenAndCloseNavBar() {
+    if (navActive == false) { //om meny inte är aktiv, öppna menyn och sätt variabel till aktiv
+        dropdownBtn.style.backgroundColor = "white";
+        dropdownBtn.style.color = "black";
+        navMenu.style.display = "block";
+        navActive = true;
+    }
+    else{ //annars stäng menyn och sätt variabel till inaktiv
+        navMenu.style.display = "none";
+        dropdownBtn.style.color = "";
+        dropdownBtn.style.backgroundColor = "";
+        navActive = false;
+    }
+}
